fd05046c81eee7056cebdca2dd79d40497677e31
Asif047 <asifulhaque047@gmail.com>
2017-11-04 19:32:32 +0000

    first commit

---
+++ OkHttpGsonTest/app/src/main/java/com/example/devpc/okhttpgsontest/ApiCall.java
@@ -0,0 +1,40 @@
+package com.example.devpc.okhttpgsontest;
+
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Created by DEVPC on 04/11/2017.
+ */
+
+public class ApiCall {
+
+    private String TAG="#RESPONSE: ";
+
+    //GET network request
+    public static String GET(OkHttpClient client, String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+        Log.d("#RESPONSE: ","INTO THE GET METHOD");
+        return response.body().string();
+    }
+
+    //POST network request
+    public static String POST(OkHttpClient client, String url, RequestBody body) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+}
---
+++ OkHttpGsonTest/app/src/main/java/com/example/devpc/okhttpgsontest/ResponseApi.java
@@ -0,0 +1,62 @@
+package com.example.devpc.okhttpgsontest;
+
+/**
+ * Created by DEVPC on 04/11/2017.
+ */
+
+public class ResponseApi {
+
+
+
+    private Integer order_id;
+
+    private String recipient;
+
+    private String order_type;
+
+    private String area;
+
+    private String district;
+
+    public Integer getOrderId() {
+        return order_id;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.order_id = order_id;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getOrderType() {
+        return order_type;
+    }
+
+    public void setOrderType(String orderType) {
+        this.order_type = order_type;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+
+}
